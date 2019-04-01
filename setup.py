cbeac8da64e7eda474a330f588350f87bbe51275
amnona <amnonim@gmail.com>
2019-04-01 15:28:14 +0300

    inital commit

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(join(dirname(__file__), 'requirements.txt'), 'r') as f:
 # version parsing from __init__ pulled from Flask's setup.py
 # https://github.com/mitsuhiko/flask/blob/master/setup.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-with open('calour/__init__.py', 'rb') as f:
+with open('dbbact_server/__init__.py', 'rb') as f:
     hit = _version_re.search(f.read().decode('utf-8')).group(1)
     version = str(ast.literal_eval(hit))
 
