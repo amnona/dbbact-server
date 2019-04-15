2ea7249e99cfd7231540427edc863b87fbd48ff6
amnona <amnonim@gmail.com>
2019-04-15 14:55:11 +0300

    make update_seq_taxonomy much faster

--- dbbact_jobs/update_seq_taxonomy.py
+++ dbbact_jobs/update_seq_taxonomy.py
@@ -23,42 +23,55 @@ def update_seq_taxonomy(con, cur, rdp='rdp_classifier_2.12/'):
         for cres in cur:
             fl.write('>%s\n%s\n' % (cres[0], cres[0]))
             num_missing_tax += 1
-    debug(2, 'found %d sequences with missing taxonomy. saved to fasta file %s' % (num_missing_tax, fasta_file_name))
+    debug(2, 'found %d sequences with missing taxonomy. saved to fasta file %s. running RDP' % (num_missing_tax, fasta_file_name))
     # run RDP
     os.system("java -Xmx1g -jar %sdist/classifier.jar classify  -o %s %s" % (rdp, output_file_name, fasta_file_name))
-    debug(2, 'RDP finished. results in %s' % output_file_name)
+    debug(2, 'RDP finished. results in %s. parsing results' % output_file_name)
 
+    # parse RDP results
     ok_levels = ['taxrootrank', 'taxdomain', 'taxphylum', 'taxclass', 'taxorder', 'taxfamily', 'taxgenus']
     ok_levels_set = set(ok_levels)
+    num_updated = 0
     with open(output_file_name) as fl:
-        for cline in output_file_name:
+        for cline in fl:
+            cline = cline.rstrip()
             ctaxonomy = [''] * len(ok_levels)
             cdat = cline.split('\t')
             cseq = cdat.pop(0)
+            # we don't use it...
+            cdirection = cdat.pop(0)
             while len(cdat) > 0:
                 cval = cdat.pop(0)
                 clevel = cdat.pop(0).lower()
                 cdblevel = 'tax' + clevel
+                try:
+                    cprob = float(cdat.pop(0))
+                except:
+                    debug(4, 'problem with probability to float')
+                    debug(4, cline.split('\t'))
+                    continue
                 if cdblevel not in ok_levels_set:
                     debug(1, 'level %s not in ok levels' % cdblevel)
+                    debug(1, cline.split('\t'))
                     continue
-                cprob = cdat.pop(0)
                 if cprob < 0.8:
                     continue
-                cur.execute('UPDATE SequencesTable SET %s=%s WHERE sequence=%s', [cdblevel, cval, cseq])
+                cur.execute('UPDATE SequencesTable SET %s=%s WHERE sequence=%s' % (cdblevel, '%s', '%s'), [cval, cseq])
                 ctaxonomy[ok_levels.index(cdblevel)] = cval
             # generate the full taxonomy string
             max_non_empty = -1
             for idx in range(len(ctaxonomy)):
                 if ctaxonomy[idx] != '':
                     max_non_empty = idx
-            if max_non_empty == -1:
+            if max_non_empty < 1:
                 taxstr = 'unknown'
             else:
                 taxstr = ';'.join(ctaxonomy[:max_non_empty + 1]) + ';'
             cur.execute('UPDATE SequencesTable SET taxonomy=%s WHERE sequence=%s', [taxstr, cseq])
+            num_updated += 1
     debug(2, 'finished updating database. committing')
     con.commit()
+    debug(3, 'done. updated taxonomy for %d sequences' % num_updated)
 
 
 def main(argv):
