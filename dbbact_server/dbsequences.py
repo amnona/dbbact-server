c9995513977461a8f8cd251f74957a7d789cadcf
amnona <amnonim@gmail.com>
2019-05-01 11:37:19 +0300

    add sequence translator support for single sequence query

--- dbbact_server/dbsequences.py
+++ dbbact_server/dbsequences.py
@@ -256,7 +256,7 @@ def GetSequenceId(con, cur, sequence, idprimer=None, no_shorter=False, no_longer
                 sid.append(resid)
 
     if seq_translate_api is not None:
-        debug(2, 'translating')
+        debug(6, 'translating')
         res = requests.post(seq_translate_api + '/test2', json={'sequences': [sequence]})
         if res.ok:
             trans_ids = res.json()['dbbact_ids'][0]
@@ -265,7 +265,7 @@ def GetSequenceId(con, cur, sequence, idprimer=None, no_shorter=False, no_longer
             trans_ids = []
         sid.extend(trans_ids)
     else:
-        debug(2, 'not translating')
+        debug(6, 'not translating')
 
     if len(sid) == 0:
         errmsg = 'sequence %s not found' % sequence
