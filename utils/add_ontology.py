e9acd6b9639cb56c0b73e8d9d0fe92a37011fd47
amnona <amnonim@gmail.com>
2021-10-14 15:50:45 +0300

    fix add_ontology.py to include ALL parents

--- utils/add_ontology.py
+++ utils/add_ontology.py
@@ -100,21 +100,32 @@ def addontology(ontofilename, ontoname, dbserver='http://127.0.0.1:7001', ontopr
 	idname = getidname(ontofilename)
 	filter_ids = get_filter_file(filter_file_name)
 	parser = oboparse.Parser(open(ontofilename))
+
+	num_non_term = 0
 	for citem in parser:
+		if citem.name.lower() != 'term':
+			num_non_term += 1
+			continue
+
 		tags = citem.tags
 		cid = tags["id"][0]
+
 		# if filtering, test if we should handle this id
 		if filter_ids:
 			if cid.split(':')[-1] not in filter_ids:
 				continue
+
+		# if no name supplied for ontology, take it from the first entry
 		if len(ontoprefix) == 0:
 			tt = cid.split(':')
 			if len(tt) > 1:
 				ontoprefix = tt[0]
+
 		# do no add obsolete terms
 		if "is_obsolete" in tags:
 			if tags["is_obsolete"][0].lower() == 'true':
 				continue
+
 		if "name" in tags:
 			origname = tags["name"][0]
 			# remove ' {' from names
@@ -123,32 +134,49 @@ def addontology(ontofilename, ontoname, dbserver='http://127.0.0.1:7001', ontopr
 		else:
 			print("ontology item id %s does not have a name" % cid)
 			origname = ''
+
+		# get the synonyms
 		if "synonym" in tags:
 			synonyms = tags["synonym"]
 		else:
 			synonyms = None
-		parent = 'NA'
-		parentid = None
+
+		# add the initial term entry with synonyms
+		data = {'term': origname, 'synonyms': synonyms, 'parent': None, 'ontologyname': ontoname, 'term_id': cid, 'parent_id': None}
+		res = requests.post(url, json=data)
+
+		# get the parents
+		parent_ids = []
 		if "is_a" in tags:
-			parentid = tags["is_a"][0]
-			# see if we have a comment ('{' before the '!')
-			if parentid is not None:
-				if parentid not in idname:
+			for parentid in tags['is_a']:
+				# see if we have a comment ('{' before the '!')
+				if parentid is not None:
+					if parentid not in idname:
+						if ' {' in parentid.split('!')[0]:
+							print('found comment (" {") for term %s' % parentid)
+							parentid = parentid.split(' {')[0]
+				parent_ids.append(parentid)
+		if "relationship" in tags:
+			for rela in tags['relationship']:
+				rela = rela.split(' ', 1)
+				if rela[0] in ['derives_from', 'located_in', 'part_of', 'develops_from', 'participates_in']:
+					parentid = rela[1]
 					if ' {' in parentid.split('!')[0]:
 						print('found comment (" {") for term %s' % parentid)
 						parentid = parentid.split(' {')[0]
-		elif "relationship" in tags:
-			rela = tags["relationship"][0]
-			rela = rela.split(' ', 1)
-			if rela[0] in ['derives_from', 'located_in', 'part_of', 'develops_from', 'participates_in']:
-				parentid = rela[1]
-		if parentid is not None:
-			if parentid in idname:
-				parent = idname[parentid]
+					parent_ids.append(parentid)
+
+		# add all parents
+		for cparent_id in parent_ids:
+			# get the parent name
+			if cparent_id in idname:
+				cparent_name = idname[cparent_id]
 			else:
-				print("parentid %s not found" % parentid)
-		data = {'term': origname, 'synonyms': synonyms, 'parent': parent, 'ontologyname': ontoname, 'term_id': cid, 'parent_id': parentid}
-		res = requests.post(url, json=data)
+				print("parentid %s not found" % cparent_id)
+				cparent_name = 'NA'
+			data = {'term': origname, 'synonyms': synonyms, 'parent': cparent_name, 'ontologyname': ontoname, 'term_id': cid, 'parent_id': cparent_id}
+			res = requests.post(url, json=data)
+	print('skipped %d non-terms' % num_non_term)
 	print('done')
 
 
